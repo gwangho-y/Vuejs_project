f69cb33b45a8bd99715651b778bdfd9de18d92e3
gwangho-y <yigwangho21@gmail.com>
2023-07-07 09:01:27 +0900

    todo 저장 기능 구현

--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="todoitem in todoItems"> {{todoitem}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  data: function () {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    // localStorage[Symbol.iterator] = function (){
+    //
+    // }
+
+    if (localStorage.length > 0) {
+      for (var i = 0; i< localStorage.length; i++) {
+        if (localStorage.key(i) === 'loglevel:webpack-dev-server') continue
+        this.todoItems.push(localStorage.key(i))
+      }
+    }
+    console.log(this.todoItems)
+  }
 }
+
+
+
 </script>
 
 <style scoped>
